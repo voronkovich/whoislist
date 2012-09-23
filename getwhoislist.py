e7b3b4fc717fbaabc7743cb807abf08d0287750b
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2012-09-24 00:00:27 +0400

    Закончена реализация основного скрипта.

--- getwhoislist.py
+++ getwhoislist.py
@@ -1,6 +1,22 @@
 from whoislist.crawler import WhoisServersListCrawler, UrlReader
 from whoislist.parser import WhoisServersListParser
+from whoislist.converter import *
+from argparse import ArgumentParser
+
+argsParser = ArgumentParser(description = "Parse WHOIS-servers list from http://www.iana.org/domains/root/db.");
+argsParser.add_argument("file", help="Set the file name where the results will be written");
+argsParser.add_argument("--xml", help="Set the output format to XML", action="store_true");
+args = argsParser.parse_args();
 
 config = { "url" : "http://www.iana.org/domains/root/db" };
 crawler = WhoisServersListCrawler(UrlReader(), WhoisServersListParser(), config);
 whoisServersList = crawler.getWhoisServersList();
+
+file = open(args.file, "w");
+if (args.xml):
+    output = convertToXml(whoisServersList);
+else:
+    output = convertToPlainText(whoisServersList);
+
+file.write(output);
+file.close();
--- whoislist/converter.py
+++ whoislist/converter.py
@@ -1,17 +1,19 @@
+import string
+
 def convertToXml(whoisServersList):
     result = [];
     result.append('<?xml version="1.0"?>\n<whoisServers>');
 
     for server in whoisServersList:
-    result.write('<server zone="' + server["domain"] + '" url="' + server["whois"] + '" />');
+        result.append('<server zone="' + server["domain"] + '" url="' + server["whois"] + '" />');
 
     result.append('</whoisServers>');
-    return result.join('\n');
+    return string.join(result, "\n");
 
 def convertToPlainText(whoisServersList, delimeter = ": "):
     result = [];
 
     for server in whoisServersList:
-        outputFile.append(server["domain"] + delimeter + server["whois"]);
+        result.append(server["domain"] + delimeter + server["whois"]);
 
-    return result.join('\n');
+    return string.join(result, '\n');
