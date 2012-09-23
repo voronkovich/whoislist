35dd04c44510c54a7ad408b135c9b0b02c9bae09
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2012-09-23 22:43:46 +0400

    Добавлен тест для функции конвертирования списка WHOIS-серверов в текст.

--- tests/convertertest.py
+++ tests/convertertest.py
@@ -12,10 +12,20 @@ class ConverterTest(WhoisServersListTestCase):
 
     def test_convertToXml_ValidData_shouldReturnXmlString(self):
         result = convertToXml(self.whoisDomainsList);
-        result = result.translate(None, '\r\n\t');
+        result = self.deleteExtraCharacters(result);
         expectedResult = self.readFixtureFileContent('whois-servers.xml');
-        expectedResult = expectedResult.translate(None, '\r\n\t');
+        expectedResult = self.deleteExtraCharacters(expectedResult);
         self.assertEqual(expectedResult, result);
+
+    def test_convertToPlainText_ValidData_shouldReturnPlainTextString(self):
+        result = convertToPlainText(self.whoisDomainsList);
+        result = self.deleteExtraCharacters(result);
+        expectedResult = self.readFixtureFileContent('whois-servers.txt');
+        expectedResult = self.deleteExtraCharacters(expectedResult);
+        self.assertEqual(expectedResult, result);
+
+    def deleteExtraCharacters(self, str):
+        return str.translate(None, '\r\n\t');
          
 if __name__ == "__main__":
     unittest.main()
