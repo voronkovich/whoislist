b84b9bdad1a040d7eec8a7ae10313355cdf73716
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2012-09-23 19:32:24 +0400

    Добавлена функция сохраняющая результаты в xml-файл.

--- getwhoislist.py
+++ getwhoislist.py
@@ -4,3 +4,16 @@ from whoislist.parser import WhoisServersListParser
 config = { "url" : "http://www.iana.org/domains/root/db" };
 crawler = WhoisServersListCrawler(UrlReader(), WhoisServersListParser(), config);
 whoisServersList = crawler.getWhoisServersList();
+saveResultsToFile("whois-servers.xml");
+
+def saveResultsToFile(fileName, domains):
+    outputFile = open(fileName, "w");
+    outputFile.write('<?xml version="1.0"?>\n');
+    outputFile.write('<servers>\n');
+
+    for domain in domains:
+        outputFile.write('<server zone="' + domain["domain"] + '" url="' + domain["whois"] + '" />\n');
+        
+    outputFile.write('</servers>');
+    outputFile.close();
+    return;
